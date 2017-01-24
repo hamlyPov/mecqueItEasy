1a73353830f19f724859a9238c1ef67c0ad0fbdc
rosoty <rosoty99@gmail.com>
2017-01-24 12:05:10 +0700

    profile

--- client/application/controller/invitelink.js
+++ client/application/controller/invitelink.js
@@ -1,4 +1,8 @@
 Template.inviteuser.helpers({
+	GetRoot:function(){
+		var roots = Iron.Location.get().rootUrl;
+		return roots;
+	},
 	GetuserInvite:function(){
 		var id = Meteor.userId();
 		var result = Meteor.users.find({'profile.affiliate':id}).map(function(document, index){
